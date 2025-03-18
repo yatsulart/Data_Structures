537eec3bfd8a84cd927301ed191a2c3bd948bbbd
reydi <yatsulart@icloud.com>
2025-03-18 17:57:56 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,28 @@
+from time import time
+
+def test(N):
+    if N == 0:
+        return "NO"
+
+    seen = [False] * N
+    rem = 0
+    start_time = time()
+
+    for i in range(1, N + 1):
+        rem = (rem * 10 + 1) % N
+        if seen[rem]:
+            end_time = time()
+            print(f"Затрачено {end_time - start_time:.6f} секунд")
+            return "NO"
+        seen[rem] = True
+        if rem == 0:
+            end_time = time()
+            print(f"Затраченое {end_time - start_time:.6f} секунд")
+            return '1' * i
+
+    end_time = time()
+    print(f"Затрачено {end_time - start_time:.6f} секунд")
+    return "NO"
+
+N = int(input("Введите N: "))
+print(test(N))
